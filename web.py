70471345ebfe9feb0b077e0a6a68139163dd98c2
kimjaeseok <tjr3117@naver.com>
2021-04-14 14:26:23 +0900

    3rd commit ( login with session )

--- web.py
+++ web.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, session
 
 app = Flask(__name__, static_url_path='/static')
 app.secret_key = b'_5#aby2L"F4Q8z\n\xec]'
@@ -18,7 +18,8 @@ def login():
 def login_confirm():
     id_ = request.form['id_']
     pw_ = request.form['pw_']
-    if id_ == 'admin' and pw_ == 'admin':
+    if (id_ == 'admin' and pw_ == 'admin') or (id_ == 'user' and pw_ == 'user'):
+        session['id'] = id_
         return redirect(url_for('index'))
     else:
         return redirect(url_for('login'))
@@ -27,6 +28,17 @@ def login_confirm():
 @app.route('/index')
 def index():
     return render_template('index.html')
+
+
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('login'))
 
 
 host_addr = "0.0.0.0"
